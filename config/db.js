1181db0040840b3c6c080b6b77891dd25daf2c09
wallandall <wallandall@gmail.com>
2020-03-07 15:14:24 +0100

    added db connection

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose');
+const config = require ('config');
+const db = config.get ('mongoURI');
+
+const connectDB = () => {
+  mongoose
+    .connect (db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then (() => console.log ('MongoDB Connected'))
+    .catch (err => {
+      console.error (erro.message);
+    });
+};
+
+module.exports = connectDB;
