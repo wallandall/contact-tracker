d5412aa9d39d706c075664f13d9bdc1fd47068b2
wallandall <wallandall@gmail.com>
2020-03-09 20:14:53 +0100

    Added Contact and ContactItem Component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -3,16 +3,13 @@ import uuid from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
-  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_CONTACTS,
   CLEAR_FILTER,
-  CONTACT_ERROR,
 } from "../types";
 
 const ContactState = props => {
@@ -21,27 +18,42 @@ const ContactState = props => {
       {
         id: 1,
         name: "Jill Johnson",
-        email: "jjohnson@gmail.com",
+        email: "jill@gmail.com",
         phone: "111-111-1111",
         type: "personal",
       },
       {
-        id: 1,
-        name: "Jack Johnson",
-        email: "jj@gmail.com",
-        phone: "111-111-1111",
+        id: 2,
+        name: "Sara Watson",
+        email: "sara@gmail.com",
+        phone: "222-222-2222",
         type: "personal",
       },
       {
-        id: 1,
-        name: "James Johnson",
-        email: "jjamesl.com",
-        phone: "111-111-1111",
-        type: "personal",
+        id: 3,
+        name: "Harry White",
+        email: "harry@gmail.com",
+        phone: "333-333-333",
+        type: "professional",
       },
     ],
   };
+
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
 
   return (
     <ContactContext.Provider
