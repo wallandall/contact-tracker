49e748021323535889aca3c7398004a34c99462f
wallandall <wallandall@gmail.com>
2020-03-13 14:32:04 +0100

    Prod Deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.use("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
